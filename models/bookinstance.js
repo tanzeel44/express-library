166aa0b09d376928e32c56b1d09ed51c7afa1a31
Tanzeel ur Rehman <tanzeel.dw@gmail.com>
2020-08-17 02:23:06 +0500

    Added dummy data to db from populatedb

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -2,16 +2,19 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const BookInstanceSchema = ({
-  book: {type: Schema.Types.ObjectId, ref: 'Book', required: true},
-  imprint: {type: String, required: true},
-  status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
-  due_back: {type: Date, default: Date.now}   
-});
+const BookInstanceSchema = new Schema(
+  {
+    book: { type: Schema.Types.ObjectId, ref: 'Book', required: true },
+    imprint: {type: String, required: true},
+    status: {type: String, required: true, enum: ['Available', 'Maintenance', 'Loaned', 'Reserved'], default: 'Maintenance'},
+    due_back: {type: Date, default: Date.now}
+  }
+);
 
-// Virtual for instance's URL
+// Virtual for URL
 BookInstanceSchema
   .virtual('url')
   .get(() => `/catalog/bookinstance/${this._id}`);
 
+//Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
